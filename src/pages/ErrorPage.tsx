e3926e5b57b32852ee4cdafc2e8456f17a0f2207
AsleyR <79556058+AsleyR@users.noreply.github.com>
2022-08-30 22:23:45 -0600

    Add Error Page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='grid justify-center text-center'>
+      <h1 className='text-7xl font-bold'>404</h1>
+      <p className='text-xl'>The Page You Were Looking For Doesn't Exist!</p>
+    </div>
+  )
+}
+
+export default ErrorPage
